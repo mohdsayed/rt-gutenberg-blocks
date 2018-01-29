55d25d33c076865bfde28524beae287120ef49b4
Sayed Taqui <sayedwp@gmail.com>
2018-01-29 17:07:19 +0530

    Configure webpack to merge css

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,61 @@
-module.exports = {
-	entry: {
-		blocks: [
-			'./blocks/slider/index.js',
-			'./blocks/simple-block/index.js',
-			'./blocks/editable-block/index.js',
-			'./blocks/bio-block/index.js',
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = [
+	{
+		entry: {
+			blocks: [
+				'./blocks/slider/index.js',
+				'./blocks/simple-block/index.js',
+				'./blocks/editable-block/index.js',
+				'./blocks/bio-block/index.js',
+			]
+		},
+		output: {
+			path: __dirname + '/js/',
+			filename: 'block.build.js',
+		},
+		module: {
+			loaders: [
+				{
+					test: /.js$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/,
+				}
+			],
+		},
+	},
+	{
+		entry: {
+			'editor.css': [
+				'./blocks/slider/editor.css',
+				'./blocks/simple-block/editor.css',
+				'./blocks/editable-block/editor.css',
+				'./blocks/bio-block/editor.css',
+			],
+			'style.css': [
+				'./blocks/slider/style.css',
+				'./blocks/simple-block/style.css',
+				'./blocks/editable-block/style.css',
+				'./blocks/bio-block/style.css',
+			]
+		},
+		output: {
+			path: __dirname + '/css/',
+			filename: '[name]',
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.css$/,
+					use: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: 'css-loader'
+					})
+				},
+			],
+		},
+		plugins: [
+			new ExtractTextPlugin( '[name]' ),
 		]
-	},
-	output: {
-		path: __dirname + '/js/',
-		filename: 'block.build.js',
-	},
-	module: {
-		loaders: [
-			{
-				test: /.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-};
+	}
+];
